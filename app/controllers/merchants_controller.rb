b24ff6d9d82d9ee12b09de6b102bb0351c70bfcf
Sanlok Lee <jlee257@berkeley.edu>
2018-04-16 22:57:31 -0700

    added record expiration

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -6,61 +6,16 @@ class MerchantsController < ApplicationController
 	def index
 		@merchants = Merchant.find_by merchant_id: 'merchants'
 
-		if @merchants.nil? 
-
-			url = 'https://jsonplaceholder.typicode.com/posts'
-			mykey = 'demo'
-			uri = URI(url)
-
-			request = Net::HTTP::Get.new(uri.path)
-			# request['Content-Type'] = 'application/xml'
-			# request['Accept'] = 'application/xml'
-			# request['X-OFFERSDB-API-KEY'] = mykey
-
-			response = Net::HTTP.new(uri.host,uri.port) do |http|
-			  http.request(request)
-			end
-
-			m = Merchant.new
-			m.merchant_id = "merchants"
-			m.merchant_info = response
-
-			if m.save
-				render json: m, status: :ok
-			else
-				render json: { :errors => m.errors.full_messages }, status: 422
-			end
-		else
-			render json: @merchants.merchant_info, status: :ok
-		end
-
-
-	end
-
-
-	# GET /merchants/:id
-	def show
-		@merchant = Merchant.find_by merchant_id: params[:id]
-
 		if @merchant.nil? 
+			puts "!!!!!!!!!!!!!!!!!! merchant is nil"
 
-			url = 'http://jsonplaceholder.typicode.com/posts/' + params[:id]
-			mykey = 'demo'
-			uri = URI(url)
+			response = fetch_server
 
-			request = Net::HTTP::Get.new(uri.path)
-			# request['Content-Type'] = 'application/xml'
-			# request['Accept'] = 'application/xml'
-			# request['X-OFFERSDB-API-KEY'] = mykey
-
-			# response = Net::HTTP.new(uri.host,uri.port) do |http|
-			#   http.request(request)
-			# end
-
-			http = Net::HTTP.new(uri.host, uri.port)
-			response = http.request(request)
-
-			puts response.body
+			if !response.is_a? Net::HTTPSuccess
+				puts "response body empty"
+				render json: { :errors => response.message }, status: response.code
+				return
+			end
 
 			m = Merchant.new
 			m.merchant_id = params[:id]
@@ -71,9 +26,109 @@ class MerchantsController < ApplicationController
 			else
 				render json: { :errors => m.errors.full_messages }, status: 422
 			end
+
+		elsif @merchant.updated_at < 10.minute.ago
+			puts "!!!!!!!!!!!!!!!!!! merchant is outdated"
+
+			response = fetch_server
+
+			if !response.is_a? Net::HTTPSuccess
+				puts "response body empty"
+				render json: { :errors => response.message }, status: response.code
+				return
+			end
+
+			@merchant.merchant_info = response.body
+
+			if @merchant.save
+				render json: @merchant.merchant_info, status: :ok
+			else
+				render json: { :errors => @merchant.errors.full_messages }, status: 422
+			end
 		else
+			puts "!!!!!!!!!!!!!!!!!! merchant is up to date"
+
+			render json: @merchants.merchant_info, status: :ok
+		end
+
+	end
+
+
+	# GET /merchants/:id
+	def show
+		@merchant = Merchant.find_by merchant_id: params[:id]
+
+		if @merchant.nil? 
+			puts "!!!!!!!!!!!!!!!!!! merchant is nil"
+
+			response = fetch_server params[:id]
+
+			if !response.is_a? Net::HTTPSuccess
+				puts "response body empty"
+				render json: { :errors => response.message }, status: response.code
+				return
+			end
+
+			m = Merchant.new
+			m.merchant_id = params[:id]
+			m.merchant_info = response.body
+
+			if m.save
+				render json: m.merchant_info, status: :ok
+			else
+				render json: { :errors => m.errors.full_messages }, status: 422
+			end
+
+		elsif @merchant.updated_at < 10.minute.ago
+			puts "!!!!!!!!!!!!!!!!!! merchant is outdated"
+
+			response = fetch_server params[:id]
+
+			if !response.is_a? Net::HTTPSuccess
+				puts "response body empty"
+				render json: { :errors => response.message }, status: response.code
+				return
+			end
+
+			@merchant.merchant_info = response.body
+
+			if @merchant.save
+				render json: @merchant.merchant_info, status: :ok
+			else
+				render json: { :errors => @merchant.errors.full_messages }, status: 422
+			end
+
+		else
+			puts "!!!!!!!!!!!!!!!!!! merchant is up to date"
+
 			render json: @merchant.merchant_info, status: :ok
 		end
 	end
 
+
+	def fetch_server(merchant_id=nil)
+
+		if merchant_id.nil?
+			puts "param is nil"
+			url = 'http://jsonplaceholder.typicode.com/posts'
+		else
+			puts "param is " + merchant_id
+			url = 'http://jsonplaceholder.typicode.com/posts/' + merchant_id
+		end
+
+		mykey = 'demo'
+		uri = URI(url)
+
+		request = Net::HTTP::Get.new(uri.path)
+		# request['Content-Type'] = 'application/xml'
+		# request['Accept'] = 'application/xml'
+		# request['X-OFFERSDB-API-KEY'] = mykey
+
+		# response = Net::HTTP.new(uri.host,uri.port) do |http|
+		#   http.request(request)
+		# end
+
+		http = Net::HTTP.new(uri.host, uri.port)
+		response = http.request(request)
+	end
 end
